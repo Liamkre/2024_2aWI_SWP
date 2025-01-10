13cf5a704b64a3d381d409217ee38c742f465143
Liam Kreigenbrink <liam.kreigenbrink@student.htldornbirn.at>
2025-01-10 14:45:01 +0100

    JS_Kennenlernen

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+
+let a = 4000;
+let b = 2000;
+let c = b / a;
+
+let s = "Hello World!";
+
+console.log("Typ der variable c ist: " + typeof c)
+console.log("Wert von der variable a ist:" + c);
+
+console.log(s);
+
